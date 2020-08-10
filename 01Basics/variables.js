a9a38f5456e7ef164447ae0c30acdf34e0768732
ADT-MAC9 <adt-mac9@ADT-MAC9s-MacBook-Pro.local>
2020-08-10 21:04:20 +0530

    Using Variables

---
+++ 01Basics/variables.js
@@ -0,0 +1,19 @@
+// In this example we'll discuss various types of data types in JS.
+
+// Defining variables
+
+// Defining string variables using let.
+let firstName = "John";
+let lastName  = "Doe"
+
+// change the first name
+firstName = "Jane"
+
+// Defining number variables using const
+const salary = "60000$"
+
+// Calling variables
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+
