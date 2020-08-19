b291f6b7b113f1f2ef0788f5be88826229736341
Atchyut Nagabhairava <atchyut.nagabhairava@adroitet.com>
2020-08-19 21:26:13 +0530

    learnt if and else conditions

---
+++ 02Basics/ifandelse.js
@@ -0,0 +1,20 @@
+//Problem to find the grade of a student based on the marks.
+
+// Conditions should be as follows:
+
+// 10 marks - Amazing
+// 5 marks - Good
+// 3 marks - poor
+// 0 marks - fail.
+
+let studentMarks = 10
+
+if(studentMarks == 10){
+  console.log("Amazing");
+}else if(studentMarks == 5) {
+  console.log("Good")
+}else if(studentMarks == 3) {
+  console.log("Poor")
+}else if(studentMarks == 0) {
+  console.log("fail")
+}
