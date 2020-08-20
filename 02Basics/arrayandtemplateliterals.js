5b92882c25023f283fa5ebad24a0e416847dfc51
Atchyut Nagabhairava <atchyut.nagabhairava@adroitet.com>
2020-08-20 15:32:34 +0530

    learnt about arrays and template literals

---
+++ 02Basics/arrayandtemplateliterals.js
@@ -0,0 +1,13 @@
+// we will write an example for an array and usage of template literals in JS.
+
+// Declaring an array
+let superHeros = ["IronMan", "SuperMan", "Thor"]
+
+// calling an array
+console.log(superHeros);
+
+// finding the length of an array
+console.log("Length of super heros:" + superHeros.length)
+
+// another way of calling a variable inside a string
+console.log(`Length of the super Heros: ${superHeros.length}`);
