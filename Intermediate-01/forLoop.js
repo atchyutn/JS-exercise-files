25bbe7d9a41c383a514efb0433e7171087bbd3f2
Atchyut Nagabhairava <atchyut.nagabhairava@adroitet.com>
2020-08-25 21:20:31 +0530

    looped days using for loop

---
+++ Intermediate-01/forLoop.js
@@ -0,0 +1,7 @@
+// we'll be looping through an array of elements and render them using for loop
+
+let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+for (let index = 0; index < days.length; index++) {
+  console.log(days[index]);
+}
