c75535e2632f805794494003ecfbc9b2f0d9f8f0
Atchyut Nagabhairava <atchyut.nagabhairava@adroitet.com>
2020-08-27 21:29:33 +0530

    created an object

---
+++ intermediate-02/objectCreation.js
@@ -0,0 +1,9 @@
+// in the exercise we'll create an object to display details of a course
+
+let courseObject = {
+  'title': 'Javascript tutorial',
+  'price': 200,
+  'author': 'Atchyut Nagabhairava'
+}
+
+console.log(`The new course named ${courseObject.title} at a price of ${courseObject.price} created by ${courseObject.author}`)
